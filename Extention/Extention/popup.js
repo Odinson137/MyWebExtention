07002f1327d2ed3502f168eddada8983ce6c2661
Odinson137 <yuriy_b@holdingbp.ru>
2024-10-15 21:18:52 +0300

    add my own extention

---
+++ Extention/Extention/popup.js
@@ -0,0 +1,14 @@
+﻿document.getElementById("replaceDomain").addEventListener("click", () => {
+    const selectedDomain = document.querySelector('input[name="domain"]:checked').value;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const currentUrl = new URL(tabs[0].url);
+        const newUrl = currentUrl.href.replace(currentUrl.host, selectedDomain);
+
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: (url) => { window.location.href = url; },
+            args: [newUrl]
+        });
+    });
+});
